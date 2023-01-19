01a4d43e3a1ab77e4e9a5b851862f42dd5e59d3e
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2023-01-20 01:19:31 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,3 +6,7 @@ mkdir -p minimal-linux/linux
 sudo apt update
 sudo apt install --yes make build-essential bc bison flex libssl-dev libelf-dev wget cpio fdisk extlinux dosfstools qemu-system-x86
 
+cd minimal-linux/build
+wget -P downloads  https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.79.tar.xz
+wget -P downloads https://busybox.net/downloads/busybox-1.35.0.tar.bz2
+

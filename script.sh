03bd914f8254d28e9a6314023d907cc38252e776
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2023-01-20 01:19:55 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,3 +10,5 @@ cd minimal-linux/build
 wget -P downloads  https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.79.tar.xz
 wget -P downloads https://busybox.net/downloads/busybox-1.35.0.tar.bz2
 
+tar -xvf downloads/linux-5.15.79.tar.xz -C sources
+tar -xjvf downloads/busybox-1.35.0.tar.bz2 -C sources

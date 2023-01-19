9ed9a0a5ffe812098291b85a7f1042b7512cf69d
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2023-01-20 01:18:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 cd ~ 
 mkdir -p minimal-linux/build/sources
-mkdir -p simple-linux/build/downloads
-mkdir -p simple-linux/build/out
-mkdir -p simple-linux/linux
+mkdir -p minimal-linux/build/downloads
+mkdir -p minimal-linux/build/out
+mkdir -p minimal-linux/linux
 sudo apt update
 sudo apt install --yes make build-essential bc bison flex libssl-dev libelf-dev wget cpio fdisk extlinux dosfstools qemu-system-x86
+
